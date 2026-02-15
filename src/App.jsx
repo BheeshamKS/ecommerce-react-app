d3fb2d51caf5f76184b950e3a70384195363270a
Bheesham505 <bheeshamkumarsajnani3@gmail.com>
2026-02-15 21:36:22 +0500

    initiating pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
-import "./App.css"
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Chechout";
+import Navbar from "./components/NavBar/Navbar";
 
 function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </div>
-  );
+    );
 }
 
 export default App;
